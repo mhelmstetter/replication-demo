40ed85eba13d9625fd8dcddb6f860e901e2d6a62
mhelmstetter <mark@helmstetter.com>
2015-02-04 09:05:12 -0500

    Organize imports

--- src/main/java/com/mongodb/flightxml/AbstractFlightTrackGenerator.java
+++ src/main/java/com/mongodb/flightxml/AbstractFlightTrackGenerator.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
 
 import javax.annotation.PostConstruct;
 
