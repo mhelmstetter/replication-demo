dc1e0e61b1a91de2f6ffe458fbf166f8ec88eb68
mhelmstetter <mark@helmstetter.com>
2015-02-10 07:04:23 -0800

    Format code, customize tooltip appearance

--- src/main/java/com/mongodb/flightdemo/DrawingContext.java
+++ src/main/java/com/mongodb/flightdemo/DrawingContext.java
@@ -1,150 +1,170 @@
 package com.mongodb.flightdemo;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JToolTip;
 
 import org.geotools.referencing.GeodeticCalculator;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class DrawingContext extends JComponent {
+public class DrawingContext extends JComponent {
 
-    protected static final Logger logger = LoggerFactory.getLogger(DrawingContext.class);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8255354107758728717L;
 
-    private final static Image GREEN_PLANE = new ImageIcon(DrawingContext.class.getResource("/images/plane-green.png")).getImage();
-    private final static Image BLUE_PLANE = new ImageIcon(DrawingContext.class.getResource("/images/plane-blue.png")).getImage();
-    private final static Image RED_PLANE = new ImageIcon(DrawingContext.class.getResource("/images/plane-red.png")).getImage();
-    private final static Image RED_DOT = new ImageIcon(DrawingContext.class.getResource("/images/dot-red.png")).getImage();
-    
-    private Image image = GREEN_PLANE;
-    private double angle = 0;
-    
-    private int delay = 0;
+	protected static final Logger logger = LoggerFactory.getLogger(DrawingContext.class);
 
-    private FlightInfo flightInfo;
+	private final static Image GREEN_PLANE = new ImageIcon(DrawingContext.class.getResource("/images/plane-green.png"))
+			.getImage();
+	private final static Image BLUE_PLANE = new ImageIcon(DrawingContext.class.getResource("/images/plane-blue.png"))
+			.getImage();
+	private final static Image RED_PLANE = new ImageIcon(DrawingContext.class.getResource("/images/plane-red.png"))
+			.getImage();
+	private final static Image RED_DOT = new ImageIcon(DrawingContext.class.getResource("/images/dot-red.png"))
+			.getImage();
 
-    private CoordinateReferenceSystem crs;
+	private Image image = GREEN_PLANE;
+	private double angle = 0;
 
-    public DrawingContext(FlightInfo flightInfo, CoordinateReferenceSystem crs) {
-        this.flightInfo = flightInfo;
-	this.crs = crs;
+	private String mouseOverLabel = null;
 
-//        if (flightInfo.getAirline() != null) {
-//            this.image = GREEN_PLANE;
-//        } else {
-//            this.image = BLUE_PLANE;
-//        }
-	
-	
+	private int delay = 0;
 
-	Dimension size = new Dimension(image.getWidth(null), image.getHeight(null));
-	setPreferredSize(size);
-	setMinimumSize(size);
-	setMaximumSize(size);
-	setSize(size);
-	setLayout(null);
-    }
+	private FlightInfo flightInfo;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((flightInfo == null) ? 0 : flightInfo.hashCode());
-        return result;
-    }
+	private CoordinateReferenceSystem crs;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+	public DrawingContext(FlightInfo flightInfo, CoordinateReferenceSystem crs) {
+		this.flightInfo = flightInfo;
+		this.crs = crs;
 
-        DrawingContext other = (DrawingContext) obj;
-        if (flightInfo == null) {
-            if (other.flightInfo != null)
-                return false;
-        } else if (!flightInfo.equals(other.flightInfo))
-            return false;
+		// if (flightInfo.getAirline() != null) {
+		// this.image = GREEN_PLANE;
+		// } else {
+		// this.image = BLUE_PLANE;
+		// }
 
-        return true;
-    }
-
-    protected void paintComponent(Graphics g) {
-	Graphics2D g2d = (Graphics2D)g;
-
-	AffineTransform tx = AffineTransform.getRotateInstance(angle, image.getWidth(null) / 2, image.getHeight(null) / 2);
-	g2d.drawImage(image, tx, null);
-    }
-
-    public void setSpritePosition(AffineTransform xx) {
-        Point2D newPositionInScreenCoordinates = xx.transform(
-                new Point2D.Double(flightInfo.getLon(), flightInfo.getLat()), null);
-
-	int newX  = (int)newPositionInScreenCoordinates.getX();
-	int newY  = (int)newPositionInScreenCoordinates.getY();
-
-	// center the image
-	newX = newX - this.image.getWidth(null) / 2;
-	newY = newY - this.image.getHeight(null) / 2;
-
-	Point currentLoc = this.getLocation();
-	Point newLoc = new Point(newX, newY);
-
-	this.setLocation(newLoc);
-    }
-
-    public Image getImage() {
-        return image;
-    }
-
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
-    public FlightInfo getFlightInfo() {
-        return flightInfo;
-    }
-
-    public void setFlightInfo(FlightInfo flightInfo) {
-        this.changePosition(flightInfo);
-    }
-
-    @Override
-    public String toString() {
-        // return "DrawingContext [spriteEnv=" + spriteEnv + ", flightInfo=" +
-        // flightInfo + "]";
-        return flightInfo.getFlightNum();
-    }
-
-    public void changePosition(FlightInfo flightInfo) {
-	if (this.flightInfo != null) {
-	    try {
-		GeodeticCalculator gc = new GeodeticCalculator(crs);
-		gc.setStartingGeographicPoint(this.flightInfo.getLon(), this.flightInfo.getLat());
-		gc.setDestinationGeographicPoint(flightInfo.getLon(), flightInfo.getLat());
-
-		// in degrees so we convert to radians
-		this.angle = gc.getAzimuth() * Math.PI / 180;
-	    } catch (IllegalArgumentException e) {
-		logger.error("Error calculating angle: " + e.getMessage());
-	    }
+		Dimension size = new Dimension(image.getWidth(null), image.getHeight(null));
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		setSize(size);
+		setLayout(null);
+		this.setToolTipText(String.format("%s %s\n%s", flightInfo.getFlightNum(), flightInfo.getAircraft(), flightInfo.getFromIata()));
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((flightInfo == null) ? 0 : flightInfo.hashCode());
+		return result;
+	}
 
-        this.flightInfo = flightInfo;        
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		DrawingContext other = (DrawingContext) obj;
+		if (flightInfo == null) {
+			if (other.flightInfo != null)
+				return false;
+		} else if (!flightInfo.equals(other.flightInfo))
+			return false;
+
+		return true;
+	}
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setColor(Color.GREEN);
+		if (mouseOverLabel != null) {
+			g2d.drawString("XXX", this.getLocation().x, this.getLocation().y);
+		}
+
+		AffineTransform tx = AffineTransform.getRotateInstance(angle, image.getWidth(null) / 2,
+				image.getHeight(null) / 2);
+		g2d.drawImage(image, tx, null);
+	}
+
+	public void setSpritePosition(AffineTransform xx) {
+		Point2D newPositionInScreenCoordinates = xx.transform(
+				new Point2D.Double(flightInfo.getLon(), flightInfo.getLat()), null);
+
+		int newX = (int) newPositionInScreenCoordinates.getX();
+		int newY = (int) newPositionInScreenCoordinates.getY();
+
+		// center the image
+		newX = newX - this.image.getWidth(null) / 2;
+		newY = newY - this.image.getHeight(null) / 2;
+
+		Point currentLoc = this.getLocation();
+		Point newLoc = new Point(newX, newY);
+
+		this.setLocation(newLoc);
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public FlightInfo getFlightInfo() {
+		return flightInfo;
+	}
+
+	public void setFlightInfo(FlightInfo flightInfo) {
+		this.changePosition(flightInfo);
+	}
+
+	@Override
+	public String toString() {
+		// return "DrawingContext [spriteEnv=" + spriteEnv + ", flightInfo=" +
+		// flightInfo + "]";
+		return flightInfo.getFlightNum();
+	}
+
+	public void changePosition(FlightInfo flightInfo) {
+		if (this.flightInfo != null) {
+			try {
+				GeodeticCalculator gc = new GeodeticCalculator(crs);
+				gc.setStartingGeographicPoint(this.flightInfo.getLon(), this.flightInfo.getLat());
+				gc.setDestinationGeographicPoint(flightInfo.getLon(), flightInfo.getLat());
+
+				// in degrees so we convert to radians
+				this.angle = gc.getAzimuth() * Math.PI / 180;
+			} catch (IllegalArgumentException e) {
+				logger.error("Error calculating angle: " + e.getMessage());
+			}
+		}
+
+		this.flightInfo = flightInfo;
+	}
 
 	public int getDelay() {
 		return delay;
@@ -155,6 +175,15 @@ class DrawingContext extends JComponent {
 		if (delay > 10) {
 			this.image = RED_PLANE;
 		}
+	}
+
+	@Override
+	public JToolTip createToolTip() {
+		JToolTip tip = super.createToolTip();
+		tip.setForeground(Color.GREEN);
+		tip.setBackground(Color.BLACK);
+		// tip.setFont(new Font("Arial", Font.BOLD,36));
+		return tip;
 	}
 
 }
